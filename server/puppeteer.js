fd607c326281440f7c1c056e159c98fb88ba141d
DCXan <chen.david.1016@gmail.com>
2022-11-06 23:27:19 -0600

    get average price from puppeteer

--- server/puppeteer.js
+++ server/puppeteer.js
@@ -1,23 +1,37 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
-    const browser = await puppeteer.launch({ headless: false })
+let averagePrice = 0;
+let url = "https://www.amazon.com/s?k=macbook+pro";
+
+async function getAveragePrice() {
+
+    const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
-    await page.goto("https://www.amazon.com/s?k=house+plants")
+    await page.goto(url)
 
     const getPrices = await page.evaluate(() => {
-        const priceTags = document.querySelectorAll(".sg-row .a-carousel-card .a-price .a-offscreen")
-        // .s-result-item.s-asin.sg-col-0-of-12.sg-col-16-of-20.sg-col.s-widget-spacing-small.sg-col-12-of-16
-
         let prices = []
+        const priceTags = document.querySelectorAll(".a-section .a-price .a-offscreen")
+
         priceTags.forEach((price) => {
-            prices.push(price.innerText)
+            let priceString = price.innerText
+            let priceFloat = parseFloat(priceString.replace("$",""))
+            prices.push(priceFloat)
         })
 
-        return prices
+        let pricesSum = prices.reduce((price, sum) => {
+            return sum + price
+        })
+
+        averagePrice = parseFloat((pricesSum / prices.length).toFixed(2))
+
+        return [prices, averagePrice]
     })
     
     console.log(getPrices)
-    // await page.screenshot({path: "puppeteer_screenshot.png"})
     await browser.close()
-})();
+}
+
+getAveragePrice(url)
+
+module.exports = averagePrice;
